3eef8245c3691603f974f1f678746f10ab718b2a
Aditi Menon <aditimenon@Aditis-MacBook-Pro.local>
2026-02-02 17:38:28 -0800

    Completed

--- Student.hpp
+++ Student.hpp
@@ -5,38 +5,48 @@
 #include <string>
 using namespace std; 
 
-class Student {
-private: 
-    string name; 
-    int age;
+class Student { // Definition of the Student class 
+private: // private data members can only be accessed within the class itself
+    string name; // stores the student's name 
+    int age; // stores the student's age 
 
-public: 
-    Student(){
-        cout << "Student object created!" << endl;
+public: // public memebers can be accessed from outside the class 
+    Student(){ 
+        cout << "Student object created!" << endl; // This is a default constructor that runs automantically when a Student object is created
+        // It also prints this message to show when a Student object is constructed 
     }
 
-    ~Student() {
+    ~Student() { //This is a destructor for the Student class 
+        // Destructor thats runs automatically when a Student object is destroyed
         cout << "Student object destroyed!" << endl; 
+        // Prints a message to show when a Student object is removed from memory
     }
 
     void setName(string s) {
+        // Setter function used to assign a value to the private name variable
         name = s; 
     }
 
 
     void setAge(int i) {
+        // Setter function used to assign a value to the private age variable 
         age = i; 
     } 
 
     string getName() const {
+        // Getter function that returns the student's name 
+        // const makes sure this function does not modify the object 
         return name; 
     }
 
 
     int getAge() const {
+        // Getter functon that returns the student's age 
+        // const makes sure this function does nto modify the object 
         return age; 
     } 
 
 }; 
 
 #endif
+
